c5404971fb879f392c0d6d2dcf9f91211d291d75
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-07-24 00:29:09 -0400

    logins

--- Code/bcds-web/src/main/webapp/scripts/app/account/login/login.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/account/login/login.controller.js
@@ -25,29 +25,3 @@ angular.module('bcdssApp')
             });
         };
     });
-
-
-        ['$scope', '$rootScope', '$location', 'AuthenticationService',
-         function ($scope, $rootScope, $location, AuthenticationService) {
-             // reset login status
-             AuthenticationService.ClearCredentials();
-      
-             $scope.login = function () {
-                 $scope.dataLoading = true;
-                 AuthenticationService.Login($scope.username, $scope.password, function(response) {
-                     if (response.success) {
-                         AuthenticationService.SetCredentials($scope.username, $scope.password);
-                         if ($scope.username == "admin") {
-                             $location.path('/admin');
-                         } else if ($scope.username == "modeler") {
-                             $location.path('/modeler');
-                         } else {
-                             $location.path('/');
-                         }
-                     } else {
-                         $scope.error = response.message;
-                         $scope.dataLoading = false;
-                     }
-                 });
-             };
-         }]);
