58cb7c413347ce49c74c7891757a297a929529a8
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-08-15 15:43:22 -0400

    updated claims controller

--- Code/bcds-web/src/main/webapp/scripts/app/claims/claims.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/claims/claims.controller.js
@@ -1,19 +1,27 @@
 'use strict';
 
-angular.module('bcdssApp').controller('ClaimsDashboardController', function($scope, $state) {
-        $scope.searchTerm = undefined;
-        //$scope.userName = "User";
+angular.module('bcdssApp').controller('RaterDashboardController', function($rootScope, $scope, $state, Account, USER_ROLE, $stateParams, ClaimService) {
+    $scope.searchTerm = undefined;
+    //$scope.USER_ROLE = USER_ROLE;
+    $scope.claims = [];
+    
+    $scope.getUserName = function(){
+    	if ($rootScope.userName != null) {
+    		$scope.userName = $rootScope.userName;
+    	} else {
+    		$scope.userName = $scope.account.firstName;
+    	}
+    };
+    $scope.getUserName();
         
-//        $scope.userName = $scope.$watch($scope.account.firstName, function(){
-//        	return $scope.account.firstName;
-//        }, true);
-        
+    $scope.loadClaims = function(){
+    	ClaimService.query(function(result){
+    		$scope.claims = result;
+    		console.log($scope.claims);
+    	});
+    };
 
-	    $scope.page = 1;
-
-	    $scope.loadPage = function(page) {
-		    $scope.page = page;
-	    };
+    $scope.loadClaims();
 
         $scope.formatDate = function(date) {
             var date = new Date(date);
@@ -46,10 +54,9 @@ angular.module('bcdssApp').controller('ClaimsDashboardController', function($sco
         }
 
         $scope.search = function(searchTerm) {
-            if(searchTerm == 1234){
+            if (searchTerm == 1234) {
                 $('#serviceErrorDialog').modal('show');
-            }
-            else {
+            } else {
                 $scope.claims.push({
                     "is_collapsed": false,
                     "file_number": "515049876",
@@ -78,6 +85,44 @@ angular.module('bcdssApp').controller('ClaimsDashboardController', function($sco
         $scope.clear = function(){
             $scope.claims = [];
         }
+        
+        $scope.isActiveRoleTab = function (userRoleTab) {
+        	$stateParams.userRoleType == userRoleTab;
+        	return userRoleTab;
+        };
+        
+        /*$scope.loadTabView = function (tabUserRole) {
+        	if(tabUserRole == USER_ROLE.userRoleRater){
+        		console.log("rater");
+        		$state.go('home', {userRoleType: USER_ROLE.userRoleRater});
+        	} else if (tabUserRole == USER_ROLE.userRoleModeler){
+        		console.log("modeler")
+        		$state.go('home', {userRoleType: USER_ROLE.userRoleModeler})
+        	} else {
+        		console.log("admin")
+        		$state.go('home', {userRoleType: USER_ROLE.userRoleAdmin})
+        	}
+        };*/
 
+        $scope.userRoleTabs = [
+                           {title: "Rater", active: $scope.isActiveRoleTab(USER_ROLE.userRoleRater)},
+                           {title: "Modeler", active: $scope.isActiveRoleTab(USER_ROLE.userRoleModeler)}, 
+                           {title: "Admin", active: $scope.isActiveRoleTab(USER_ROLE.userRoleAdmin)}
+                       ];
 
+        $scope.loadRaterTab = function () {
+        	console.log("rater");
+        	console.log(USER_ROLE);
+            $state.go('home', {userRoleType: USER_ROLE.userRoleRater});
+        };
+
+        $scope.loadModelerTab = function () {
+        	console.log("modeler");
+            $state.go('home', {userRoleType: USER_ROLE.userRoleModeler});
+        };
+
+        $scope.loadAdminTab = function () {
+        	console.log("admin");
+            $state.go('home', {userRoleType: USER_ROLE.userRoleAdmin});
+        };
     });
