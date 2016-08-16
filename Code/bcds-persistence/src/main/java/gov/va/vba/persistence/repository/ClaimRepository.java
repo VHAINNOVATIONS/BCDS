f5f9f4768050e3a6d8e5842fba96fa45aa59d959
unknown <Habibm@xx-3093.pro-spheretek.com>
2016-08-16 09:40:10 -0400

    A native query was added to limit the row and increase the performance

--- Code/bcds-persistence/src/main/java/gov/va/vba/persistence/repository/ClaimRepository.java
+++ Code/bcds-persistence/src/main/java/gov/va/vba/persistence/repository/ClaimRepository.java
@@ -1,6 +1,9 @@
 package gov.va.vba.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import gov.va.vba.persistence.entity.Claim;
 
@@ -9,6 +12,7 @@ import gov.va.vba.persistence.entity.Claim;
  */
 public interface ClaimRepository extends JpaRepository<Claim, Long> {
 
-    
+	@Query(value = "SELECT PTCPNT_VET_ID, CLAIM_RO_NAME, BNFT_CLAIM_ID, DATE_OF_CLAIM, CNTNTN_CLMANT_TXT FROM BCDSS.AH4929_RATING_CORP_CLAIM WHERE CNTNTN_CLMANT_TXT LIKE '%KNEE%' OR CNTNTN_CLMANT_TXT LIKE '%EAR%'", nativeQuery = true)
+	public List<Claim> findAll();
 
 }
