93c8bdbccb0ae3603ea79a01d7b1ebf4d0427719
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-08-18 16:04:54 -0400

    dashboard field clean up

--- Code/bcds-web/src/main/webapp/scripts/app/modeler/modeler.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/modeler/modeler.controller.js
@@ -2,53 +2,7 @@
 
 angular.module('bcdssApp').controller('ModelerDashboardController', function($scope, $state) {
         $scope.searchTerm = undefined;
-        //$scope.userName = "User";
         
-//        $scope.userName = $scope.$watch($scope.account.firstName, function(){
-//        	return $scope.account.firstName;
-//        }, true);
-        
-        $scope.claims = [{
-            "is_collapsed": false,
-            "veteranId" : 212029876,
-            "veteranName": "John Doe",
-            "regionalOffice": "Houston, TX",
-            "claimNumber": 20160621,
-            "dateOfClaim": "03/10/2016",
-            "cestDate": "03/10/2016",
-            "contentions": [{
-                "code": 2200,
-                "description": "Hand",
-                "lastModel": "03/10/2016",
-            }, {
-                "code": 2210,
-                "description": "Ear",
-                "lastModel": "03/10/2016"
-            }]
-	    }, {
-            "is_collapsed": false,
-            "veteranId" : 900125836,
-            "veteranName": "Albert Smith",
-            "regionalOffice": "New York City, NY",
-            "claimNumber": 20160516,
-            "dateOfClaim": "03/10/2016",
-            "cestDate": "03/10/2016",
-            "contentions": [{
-                "code": 2250,
-                "description": "Elbow",
-                "lastModel": "03/10/2016",
-            }, {
-                "code": 2220,
-                "description": "Finger",
-                "lastModel": "03/10/2016"
-            }, {
-                "code": 2230,
-                "description": "Arm",
-                "lastModel": "03/10/2016"
-            }]
-	    }
-	    
-	    ];
 	    $scope.page = 1;
 
 	    $scope.loadPage = function(page) {
@@ -84,40 +38,5 @@ angular.module('bcdssApp').controller('ModelerDashboardController', function($sc
         $scope.click = function() {
             $('#next').removeClass('hidden');
         }
-
-        $scope.search = function(searchTerm) {
-            if(searchTerm == 1234){
-                $('#serviceErrorDialog').modal('show');
-            }
-            else {
-                $scope.claims.push({
-                    "is_collapsed": false,
-                    "file_number": "515049876",
-                    "name": "John Smith",
-                    "claim_num": "5625193",
-                    "claim_date": "01/10/2016",
-                    "contentions": [
-                        {
-                            "code": 5100,
-                            "description": "Knee"
-                        },
-                        {
-                            "code": 2100,
-                            "description": "Ear"
-                        }
-                    ]
-                });
-            }
-        }
-
-        $scope.deleteClaim = function(claim) {
-            var index = $scope.claims.indexOf(claim);
-            $scope.claims.splice(index,1);
-        }
-
-        $scope.clear = function(){
-            $scope.claims = [];
-        }
-
 
     });
