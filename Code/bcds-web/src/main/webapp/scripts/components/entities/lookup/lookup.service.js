8cb95f2326b5860b02c2a4712c795b4aaf36e432
unknown <Habibm@xx-3093.pro-spheretek.com>
2016-08-19 10:25:15 -0400

    Cleanup unused items

--- Code/bcds-web/src/main/webapp/scripts/components/entities/lookup/lookup.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('bcdssApp')
-    .factory('Lookup', function ($resource, DateUtils) {
-        return $resource('api/lookup', {type: '@type', referenceId: '@referenceId'}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
