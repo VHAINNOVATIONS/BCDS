f8ef9596225467a64a8f45dec0b669f8a8105a64
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-08-25 11:38:39 -0400

    update url

--- Code/bcds-web/src/main/webapp/scripts/app/admin/admin.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/admin/admin.controller.js
@@ -3,7 +3,7 @@
 angular.module('bcdssApp').controller('AdminDashboardController', function($rootScope, $scope, $state, $stateParams, $http) {
 	$scope.users = [];
 
-	$http.get('http://localhost:8083/bcds-web-0.0.1-SNAPSHOT-tomcat/api/users').
+	$http.get('/bcds-web-0.0.1-SNAPSHOT-tomcat/api/users').
 		success(function(data) {
 			$scope.users = data;
 		});
