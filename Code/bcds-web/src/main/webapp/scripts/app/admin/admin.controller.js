b472cda7c616e5816372dc06c412c10d1251b9e1
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-08-25 09:59:15 -0400

    defect fix

--- Code/bcds-web/src/main/webapp/scripts/app/admin/admin.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/admin/admin.controller.js
@@ -1,65 +1,14 @@
 'use strict';
 
-angular.module('bcdssApp').controller('AdminDashboardController', function($rootScope, $scope, $state, Account, /*USER_ROLE,*/ $stateParams) {
-    $scope.searchTerm = undefined;
-    //$scope.USER_ROLE = USER_ROLE;
-    
-    $scope.getUserName = function(){
-    	if ($rootScope.userName != null) {
-    		$scope.userName = $rootScope.userName;
-    	} else {
-    		$scope.userName = $scope.account.firstName;
-    	}
-    };
-    $scope.getUserName();
-        
-        $scope.isCollapsed = function(claim) {
-            if (claim.isCollapsed == undefined) {
-                claim.isCollapsed = true;
-            }
-            return claim.isCollapsed;
-        };
+angular.module('bcdssApp').controller('AdminDashboardController', function($rootScope, $scope, $state, $stateParams, UserRepository) {
+	$scope.users = [];
 
-        $scope.toggleCollapse = function(claim) {
-            if (claim.isCollapsed == undefined) {
-                claim.isCollapsed = true;
-            } else {
-                claim.isCollapsed = !claim.isCollapsed;
-            }
-        };
+	$scope.getUsers = function() {
+		UserRepository.getAllUsers(function (results) {
+			$scope.users = results;
+		})
+	}
+	
+	$scope.getUsers();
 
-        $scope.displayProcess = function() {
-          $('#modelDialog').modal('show');
-        };
-
-        $scope.click = function() {
-            $('#next').removeClass('hidden');
-        }
-
-        $scope.isActiveRoleTab = function (userRoleTab) {
-        	$stateParams.userRoleType == userRoleTab;
-        	return userRoleTab;
-        };
-        
-/*        $scope.userRoleTabs = [
-                           {title: "Rater", active: $scope.isActiveRoleTab(USER_ROLE.userRoleRater)},
-                           {title: "Modeler", active: $scope.isActiveRoleTab(USER_ROLE.userRoleModeler)}, 
-                           {title: "Admin", active: $scope.isActiveRoleTab(USER_ROLE.userRoleAdmin)}
-                       ];
-
-        $scope.loadRaterTab = function () {
-        	console.log("rater");
-        	console.log(USER_ROLE);
-            $state.go('home', {userRoleType: USER_ROLE.userRoleRater});
-        };
-
-        $scope.loadModelerTab = function () {
-        	console.log("modeler");
-            $state.go('home', {userRoleType: USER_ROLE.userRoleModeler});
-        };
-
-        $scope.loadAdminTab = function () {
-        	console.log("admin");
-            $state.go('home', {userRoleType: USER_ROLE.userRoleAdmin});
-        };*/
-    });
+});
