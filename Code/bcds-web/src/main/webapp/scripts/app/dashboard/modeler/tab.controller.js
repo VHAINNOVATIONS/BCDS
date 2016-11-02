1e4ae470b5fe6d1fcd948118c7d9f0ae811d7ed5
varunneelakantan <29n.varun@gmail.com>
2016-11-01 23:51:33 -0400

    VABCDS-692 Navigation redesign for rater and modeler screens

--- Code/bcds-web/src/main/webapp/scripts/app/dashboard/modeler/tab.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/dashboard/modeler/tab.controller.js
@@ -2,14 +2,23 @@
 
 angular.module('bcdssApp')
   .controller('TabController', ['$scope', function($scope) {
-    $scope.tab = 1;
-
-    $scope.setTab = function(newTab){
-      $scope.tab = newTab;
-    };
-
-    $scope.isSet = function(tabNum){
-      return $scope.tab === tabNum;
-    };
+	  	$scope.tab = 1;
+		$scope.Dashboardtab = 1;
+		
+		$scope.setTab = function(newTab){
+		  $scope.tab = newTab;	  
+		};
+		
+		$scope.setDashboardTab = function(tabName){
+			$scope.Dashboardtab = tabName;
+		}
+		
+		$scope.isDashboardTabSet = function(tabNum){
+			return $scope.Dashboardtab === tabNum;
+		}
+		
+		$scope.isSet = function(tabNum){
+		  return $scope.tab === tabNum;
+		};
 }]);
 
