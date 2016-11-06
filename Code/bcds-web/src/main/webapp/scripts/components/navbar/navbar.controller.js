46f0b8dfc16641586060bee851ac8a9ae7d93b9b
varunneelakantan <29n.varun@gmail.com>
2016-11-06 00:29:55 -0400

    VABCDS-692 Redesign of navigation menu

--- Code/bcds-web/src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -4,7 +4,8 @@ angular.module('bcdssApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
-
+        $scope.userName = Principal.userName;
+        
         $scope.logout = function () {
             Auth.logout();
             $state.go('home');
