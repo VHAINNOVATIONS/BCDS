381d5facdeb74d63892d808bd0d2c7805eb45600
dominicyeh <dominic.yeh@pro-spheretek.com>
2016-11-08 12:35:30 -0500

    update identifier

--- Code/bcds-persistence/src/main/java/gov/va/vba/persistence/entity/DDMDiagnosis.java
+++ Code/bcds-persistence/src/main/java/gov/va/vba/persistence/entity/DDMDiagnosis.java
@@ -2,10 +2,16 @@ package gov.va.vba.persistence.entity;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
@@ -13,6 +19,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Table(schema="BCDSS", name = "DDM_DIAG")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class DDMDiagnosis implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Long diagnosisId;
 	private Long categoryId;
@@ -22,6 +30,7 @@ public class DDMDiagnosis implements Serializable {
 	private Date createdDate;
 	private String modelType;
 
+	@Id
 	@Column(name="DIAG_ID")
 	public Long getDiagnosisId() {
 		return diagnosisId;
