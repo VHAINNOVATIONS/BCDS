8c25d3b231df22f5a4f0b009590631cb7556efd3
NAM\asingh347 <ajit.x.singh@us.pwc.com>
2016-12-09 15:38:31 -0500

    front end work for story 199-750

--- Code/bcds-web/src/main/webapp/scripts/components/entities/rating/rating.service.js
+++ Code/bcds-web/src/main/webapp/scripts/components/entities/rating/rating.service.js
@@ -1,25 +1,27 @@
 'use strict';
 
 angular.module('bcdssApp')
-    .factory('RatingService', function ($resource, DateUtils) {
-        return $resource('api/results', {}, {
-        	'query': {method: 'GET', isArray: true},
-            'get': {
-                method: 'GET',
-                transformResponse: function (data) {
-                    data = angular.fromJson(data);
-                    return data;
-                }},
-            'findModelRatingResults': {
-                url: 'api/modelRatingResults', 
-                method: 'POST', 
-                data = {
-                        processId : 1
-                },
-                transformResponse: function (data) {
-                    console.log(data);
-                    data = angular.fromJson(data);
-                    return data;
-                }}
-        });
+    .factory('RatingService', function ($http, $resource, DateUtils) {
+        return {
+            getRatingResults: function(){
+                return $http.get('api/results')
+                    .then(function (response) {
+                        return angular.fromJson(response);
+                    });
+            },
+
+            findModelRatingResults: function (filters) {
+                console.log(filters);
+                var data = {
+                        processId : 1, //filters.modelResultId,           
+                        fromDate: filters.fromDate,
+                        toDate: filters.toDate,
+                        modelType: filters.regionalOfficeOption
+                }
+                return $http.post('api/modelRatingResults', data)
+                    .then(function (response) {
+                        return angular.fromJson(response);
+                    });
+            }
+        };
     });
