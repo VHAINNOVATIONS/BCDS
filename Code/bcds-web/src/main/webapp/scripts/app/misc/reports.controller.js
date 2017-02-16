2972a77034091bdc703e356f8f3dc7678a99fbd4
NAM\asingh347 <ajit.x.singh@us.pwc.com>
2017-02-16 09:30:36 -0500

    promise support for IE

--- Code/bcds-web/src/main/webapp/scripts/app/misc/reports.controller.js
+++ Code/bcds-web/src/main/webapp/scripts/app/misc/reports.controller.js
@@ -110,7 +110,7 @@ angular.module('bcdssApp').controller('ReportsController', function($rootScope, 
               orientation: 'landscape',
               pageSize: 'LEGAL',
               exportOptions: {
-                  columns: ':visible',
+                  //columns: ':visible',
                   columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 , 10 , 11, 12, 13, 14]
               },
               init: function(dt, node, config) {
